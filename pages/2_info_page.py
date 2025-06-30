9bf088f75b83a6e947c1151216b2d2f1a074ec7d
neelm0703 <mehta319@purdue.edu>
2025-06-30 11:19:06 +0530

    Update 2_info_page.py

--- pages/2_info_page.py
+++ pages/2_info_page.py
@@ -69,7 +69,7 @@ def doctors_nearby(query, location):
                 })
             return pd.DataFrame(places)
         elif data["status"] == "ZERO_RESULTS":
-            st.warning("No results found!")
+            col2.error("There was a problem while retrieving results. Please try again later.")
         else:
             st.error("Error with API")
             st.write("Raw API response:", data)
@@ -117,7 +117,6 @@ def create_map(column):
         col2.write("Specialists Nearby:")
         df = doctors_nearby("doctor for " + st.session_state["selected_disease"], location)
         if (not df):
-            st.error("There was a problem while retrieving results. Please try again later.")
             return None
             
         df = df.dropna(subset = ['Latitude', 'Longitude'])
