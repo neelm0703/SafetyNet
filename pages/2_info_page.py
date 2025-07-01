fd71e7eb6ac966f7355f59342d7363b6ce07dfe6
SeiyaMutreja <73780380+SeiyaMutreja@users.noreply.github.com>
2025-07-01 11:15:46 -0500

    Update 2_info_page.py

--- pages/2_info_page.py
+++ pages/2_info_page.py
@@ -55,12 +55,6 @@ def doctors_nearby(query, location):
         res.raise_for_status()
         data = res.json()
 
-        st.subheader("Raw API Response (Local Debugging):")
-        st.json(data)
-        print("\n--- Full API Response (Local) ---")
-        print(json.dumps(data, indent=2))
-        print("---------------------------------\n")
-
         if data["status"] == "OK":
             places = []
             for place in data.get("results", []):
